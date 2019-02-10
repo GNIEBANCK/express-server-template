791951885b124ed0f7bfd47321386a9b698bf41e
Gregory Niebanck <gniebanc@vt.edu>
2019-02-10 11:12:33 -0500

    more basics

--- api/handlers/getHandlers.js
+++ api/handlers/getHandlers.js
@@ -1,29 +1,17 @@
 'use strict';
-exports.getEntity = function (req, res) {
-	// path parameters
-	var pathParam = req.params.pathParam;
-	// query parameters
-	for (var param in req.query) {
-		query_key = param;
-		query_value = req.query[param];
+exports.getParams = function (req, res) {
+	try {
+		let results = {};
+		// path parameters
+		results[pathParam] = req.params.pathParam;
+		// query parameters
+		for (var param in req.query) {
+			results[param] = req.query[param];
+		}
+		res.status(200).json(results);
 	}
-
-	var request = require('request');
-	var yahoo_req = {};
-	yahoo_req.url = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + zip + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-	request.get(yahoo_req, function (error, response, body) {
-		if (error) {
-			res.status(400).send({ error: "Bad Request" });
-		}
-		else {
-			var body_obj = JSON.parse(body);
-			var temp = Number(body_obj.query.results.channel.item.condition.temp);
-		}
-		var response_object = {};
-		if (query_value === "Celsius") temp = Math.round((temp - 32) / 1.8);
-		response_object.temperature = temp;
-		response_object.scale = query_value;
-		res.status(200).json(response_object);
-	});
+	catch (error) {
+		res.status(500).send({ error: "An error has occurred" });
+	}
 };
-	
+
--- api/router.js
+++ api/router.js
@@ -2,5 +2,5 @@
 module.exports = function(app) {
   // link handlers
   var getHandlers = require('.handlers/getHandlers');
-  app.route('/entity/:queryParam').get(getHandlers.getEntity);
+  app.route('/entity/:pathParam').get(getHandlers.getParams);
 };
